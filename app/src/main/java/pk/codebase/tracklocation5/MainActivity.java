51523eb7f67f1a3e97c0b28304ca8785ea712577
root <root@DESKTOP-GJKGO23.localdomain>
2020-08-27 13:49:57 +0500

    First Comment

---
+++ app/src/main/java/pk/codebase/tracklocation5/MainActivity.java
@@ -0,0 +1,26 @@
+package pk.codebase.tracklocation5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn_Location = findViewById(R.id.btn_Location);
+        btn_Location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
